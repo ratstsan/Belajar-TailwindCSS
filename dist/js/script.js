86bb6aeff152340397762167c2f009f8e37ffa01
ratstsan <aratstsan@gmail.com>
2024-01-31 11:25:46 +0700

    Third Commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,6 +18,7 @@ window.onscroll = function () {
 // Hamburger
 const hamburger = document.querySelector('#hamburger')
 const navMenu = document.querySelector('#nav-menu')
+const modeDark = document.querySelector('#mode')
 
 hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active')
@@ -26,8 +27,41 @@ hamburger.addEventListener('click', function () {
 
 // klik diluar hamburger
 window.addEventListener('click', function (e) {
-    if (e.target != hamburger && e.target != navMenu) {
+    if (e.target != hamburger && e.target != navMenu && e.target != modeDark) {
         hamburger.classList.remove('hamburger-active')
         navMenu.classList.add('hidden')
     }
 })
+
+// Darkmode Toggle
+const darkToggle = document.querySelector('#dark-toggle')
+const html = document.querySelector('html')
+const sunToggle = document.querySelector('#sun')
+const moonToggle = document.querySelector('#moon')
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark')
+        localStorage.theme = 'dark'
+        sunToggle.classList.add('hidden')
+        moonToggle.classList.remove('hidden')
+    } else {
+        html.classList.remove('dark')
+        localStorage.theme = 'light'
+        moonToggle.classList.add('hidden')
+        sunToggle.classList.remove('hidden')
+    }
+})
+
+// Refresh dark mode
+if (
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+    darkToggle.checked = true
+    sunToggle.classList.add('hidden')
+} else {
+    darkToggle.checked = false
+    moonToggle.classList.add('hidden')
+}
